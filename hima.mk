4d303a997bef08851764c6eb875887c96bdadfa9
Ethan Chen <intervigil@gmail.com>
2015-11-30 21:23:54 -0800

    hima: Add camera and RIL shim libraries
    Change-Id: Ia0321e686405e9a00754d3260a555b86ec1d82f9

--- hima.mk
+++ hima.mk
@@ -85,6 +85,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
+# Camera
+PRODUCT_PACKAGES += \
+    libcamera_shim
+
 # Compat symbols
 PRODUCT_PACKAGES += \
     libstlport
@@ -206,6 +210,10 @@ PRODUCT_COPY_FILES += \
 # Recovery
 PRODUCT_PACKAGES += \
     librecovery_updater_hima
+
+# RIL
+PRODUCT_PACKAGES += \
+    libril_shim
 
 # Sensors
 PRODUCT_COPY_FILES += \
